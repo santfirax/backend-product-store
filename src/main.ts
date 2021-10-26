5ee2018d525887b252ed283db089b3b21433a47f
Santiago Molano <santi-1524@hotmail.com>
2021-10-26 08:21:35 -0500

    Add swagger and Fix ssl connection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error'],
   });
-  await app.listen(3000);
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Backend product store')
+    .setDescription('Product Store API docs')
+    .setVersion('1.0')
+    .build();
+    
+  const swaggerDocs = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup("docs",app,swaggerDocs);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
